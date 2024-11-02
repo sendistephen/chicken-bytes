81af3a05f3d29c4c19a0b51d75ead6e5cab5492a
sendistephen <sendi.stev@gmail.com>
2024-11-02 15:04:01 +0300

    feat: setup TS type checks, ESLint, Prettier, Commitlint and Husk
    Sets up package.json script to do TS type checks. Set up prettier and ESLint with good rules.
    Configures Husyk and sets up Commitizenr  using the  conventional commit standard.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="text-xl font-bold text-blue-500">Hello</div>;
+  return <div className='text-xl font-bold text-blue-500'>Hello</div>;
 }
---
+++ commitlint.config.cjs
@@ -0,0 +1,9 @@
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "references-empty": [1, "never"],
+    "footer-max-line-length": [0, "always"],
+    "body-max-line-length": [0, "always"],
+  },
+};
+module.exports = config;
